b6903524e31c0845b23123d75f783f97af04e8a2
FiliKiliBakarKrade <143165255+FiliKiliBakarKrade@users.noreply.github.com>
2023-08-25 12:23:51 +0200

    Dugme_class+clickdetection.py

--- Dugme_class.py
+++ Dugme_class.py
@@ -14,7 +14,7 @@ class Button:
     self.lightcolor = lightcolor
     self.textcolor = textcolor
 
-dugme1 = Button(300, 200, 250, 100, "red", "dugme", "pink", "aquamarine")
+dugme1 = Button(300, 200, 250, 100, "red", "dugme", "pink", "darkblue")
 
 
 clock = pg.time.Clock
@@ -23,7 +23,9 @@ WIDTH = 640
 HEIGHT = 480
 screen = pg.display.set_mode((WIDTH , HEIGHT))
 
-def drawdugme(dugme, highlight):
+def drawdugme(dugme, highlight,klik):
+        if klik:
+            dugme1.text = "kliknuto"
         if not highlight:
             pg.draw.rect(screen, dugme.color, (dugme.centerx-dugme.W/2, dugme.centery-dugme.H/2, dugme.W, dugme.H))
             font1 = pg.font.SysFont('Comic Sans MS', math.floor(dugme.H*0.8))
@@ -40,8 +42,12 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+    screen.fill("white")
+    dugme1.text = "dugme"
     highlight = False
     if pg.mouse.get_pos()[0]> dugme1.centerx-dugme1.W/2 and pg.mouse.get_pos()[0]< dugme1.centerx+dugme1.W/2 and pg.mouse.get_pos()[1]> dugme1.centery-dugme1.H/2 and pg.mouse.get_pos()[1]< dugme1.centery+dugme1.H/2:
         highlight = True
-    drawdugme(dugme1, highlight)
+    klik = pg.mouse.get_pressed()[0]
+    klik = klik and highlight
+    drawdugme(dugme1, highlight, klik)
     
