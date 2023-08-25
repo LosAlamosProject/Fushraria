ccb0ca3a1cf0c7fa0b478bad6fe9130dfa1d6313
FiliKiliBakarKrade <143165255+FiliKiliBakarKrade@users.noreply.github.com>
2023-08-25 13:17:40 +0200

    Update Character_class.py

--- Character_class.py
+++ Character_class.py
@@ -1,13 +1,17 @@
 class Character:
-  def __init__(self, posX, posY, health, stamina, isInAir, W, H, skin, defence, isOnIce, isInWater, o2):
+  def __init__(self, posX, posY, health, stamina, defence, dmg, W, H, skin,  isInAir, isOnIce, isInWater, isFlying, isSprinting, o2):
     self.centerx = posX
     self.centery = posY
     self.W = W
     self.H = H
     self.health = health
     self.stamina = stamina
+    self.defence = defence
+    self.dmg = dmg
     self.isInAir = isInAir
     self.skin = skin
-    self.isInAir = isOnIce
-    self.isInAir = isInWater
+    self.isOnice = isOnIce
+    self.isInWater = isInWater
+    self.isFlying = isFlying
+    self.isSprinting = isSprinting
     self.o2 = o2
