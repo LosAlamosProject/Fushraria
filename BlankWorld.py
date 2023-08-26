66cc354b6a0ffe8867534fb95108a63b50bc0346
vekidev <93841364+vekidev@users.noreply.github.com>
2023-08-26 09:55:11 +0200

    noise generation pt.2

--- BlankWorld.py
+++ BlankWorld.py
@@ -1,6 +1,7 @@
 import pygame as pg
-import sys
-import keyboard
+import sys, random
+import noise
+
 class Character:
   def __init__(self, posX, posY, vX, vY ,health, stamina, defence, dmg, W, H, skin,  isInAir, isOnIce, isInWater, isFlying, isSprinting, o2):
     self.posX = posX
@@ -24,22 +25,59 @@ pg.init()
 screen = pg.display.set_mode((800, 600))
 clock = pg.time.Clock()
     # Create the character
-character = Character(100, 450, 0, 0, 100, 100, 10, 10, 50, 100, "player.png", False, False, False, False, False, 0)
+character = Character(100, 250, 0, 0, 100, 100, 10, 10, 50, 100, "player.png", True, False, False, False, False, 0)
     # Create the ground
 ground = pg.Rect(0, 500, 800, 100)
     # Main game loop
  
+yd = 0
+
+world = [[0 for i in range(100)] for i in range(100)]
+
+startx = random.randint(1,10000)
+for x, tile in enumerate(world[0]):
+    gen = noise.pnoise1(x * 0.1 + startx, repeat=999999999)
+    if gen >= 0: 
+        for i in range(int(gen * 10) + 10,100): world[i][x] = 1
+    elif gen < 0: 
+        for i in range(0-int(gen * 10) + 10,100): world[i][x] = 1
+
 def draw():
+    global yd
     screen.fill("black")
-    pg.draw.rect(screen, "white", ground)
+    # yd -= 1
+    # pg.draw.rect(screen, "white", ground)
     pg.draw.rect(screen, "red", (character.posX-character.W/2, character.posY-character.H/2, character.W, character.H))
-    pg.display.update()
+
+    
+
+
+    for y, row in enumerate(world):
+        for x, tile in enumerate(row):
+            
+            if tile == 1:
+                pg.draw.rect(screen,(255,255,255),(x*32,y*32-yd,32,32))
+
+    pg.display.flip()
  
- 
- 
- 
+
+
+# generated = noisegenerator.generate(100)
+# for x, i in enumerate(generated):
+#     if i > 0:
+#         for y in range(int(i) * 3,99):
+#             world[int(y)][x] = 1
+#     else:
+#         for y in range(int(-i) * 3,99):
+#             world[int(y)][x] = 1
+
+velocityX = 0
 velocityY = 0
 while True:
+        # dt = clock.tick(60) / 1000
+
+        character.posX += velocityX
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
@@ -47,11 +85,16 @@ while True:
         if character.posY >= ground.top - character.H/2:
             character.isInAir = False
             velocityY = 0
-        if keyboard.is_pressed("A"):
-            character.posX -= 5 #PLAYER.SPEED
-        if keyboard.is_pressed("D"):
-            character.posX += 5 #PLAYER>SPEED
-        if keyboard.is_pressed(" ") and character.isInAir == False:
+
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_a]:
+            velocityX = -5 #PLAYER.SPEED
+        elif keys[pg.K_d]:
+            velocityX = 5 #PLAYER>SPEED
+        else:
+            velocityX = 0
+        if keys[pg.K_SPACE] and character.isInAir == False:
             velocityY -= 15 #PLAYER.JUMPHEIGHT
             character.isInAir = True
         velocityY += 1 #GRAVITY
@@ -60,5 +103,5 @@ while True:
             character.posY += velocityY
         # Update the character's position
         draw()
- 
+
         clock.tick(60)
--- noisegenerator.py
+++ noisegenerator.py
@@ -3,6 +3,8 @@ import noise, random
 # TODO: napravi funkciju generate koja generise 2d platformer svet
 # na noise algoritmu.
 
+# unused, ne koristiti
+
 def generate(width : int = 69) -> list:
     heights = []
 
@@ -12,6 +14,6 @@ def generate(width : int = 69) -> list:
 
     for i in range(width):
         x = float(i) * span / points - 0.5 * span
-        heights.append(noise.pnoise1(x + startpos + i) * 10)
+        heights.append(noise.pnoise1(x + startpos + i) * 5)
 
     return heights
